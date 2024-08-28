6ecb51d89d3009160a9f4debfa2cb87a8739b5ff
MuhammadArsalanShah <arsalanshah408@yahoo.com>
2024-08-28 18:25:48 +0500

    Using CSS files as modules with the help of React's build process i-e vite

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import logo from '../assets/logo.png';
-import './Header.css';
+import styles from './Header.module.css';
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>React Art</h1>
-      <p /*style={{color: 'red', fontStyle: 'italic'}}*/>A community of artists and art-lovers.</p>
+      <p>A community of artists and art-lovers.</p>
     </header>
   );
 }
