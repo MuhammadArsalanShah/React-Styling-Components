7237700422458975166ee9e1003e8874a39d3f9e
MuhammadArsalanShah <arsalanshah408@yahoo.com>
2024-08-28 16:59:00 +0500

    Coding exercise 17: Working with inline dynamic styling

--- src/App.jsx
+++ src/App.jsx
@@ -11,3 +11,65 @@ export default function App() {
     </>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/***** Coding Exercise 17 *****/
+/* 
+import React from 'react';
+
+function App() {
+    const[color, setColor] = React.useState('white')
+    
+    
+    function handleChange(userResponse) {
+        if (userResponse === 'yes') {
+            setColor('green')
+        }
+        if (userResponse === 'no') {
+            setColor('red')
+        }
+    }
+    
+    return (
+    <div id="app">
+      <h1 style={{ color: color}}>CSS is great!</h1>
+      <menu>
+        <li>
+          <button onClick={() => handleChange('yes')}>Yes</button>
+        </li>
+        <li>
+          <button onClick={() => handleChange('no')}>No</button>
+        </li>
+      </menu>
+    </div>
+  );
+}
+
+export default App; 
+*/
+
